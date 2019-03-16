68dd503ae1e518ca1ebb551a9d0475ca7d4bebb4
Katie Blizzard <kblizzard11@gmail.com>
2019-03-16 02:10:18 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,8 +1,14 @@
 class Anagram
+  
+  attr_accessor :word 
+  
+  def initialize(word) 
+    self.word = word 
+  end 
 
-def match(anagram_array)
-
-end 
+  def match(anagram_array)
+  
+  end 
 
 
 end 
