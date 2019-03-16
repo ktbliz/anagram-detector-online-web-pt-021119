bd260ab0dcca3845f7be2680f0297fff71298a68
Katie Blizzard <kblizzard11@gmail.com>
2019-03-16 02:15:49 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,8 +6,15 @@ class Anagram
     self.word = word 
   end 
 
-  def match(anagram_array)
-  
+  def match(anagrams_array)
+    word_chars_array = self.word.split("").sort 
+    matches_array = []
+    anagrams_array.each do |anagram|
+      if anagram.split("").sort == word_chars_array
+        matches_array << anagram
+      end 
+    end 
+    
   end 
 
 
